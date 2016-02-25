9d193a2d93b02722609150e7544cda416afbef33
dkaitou <dkaitou@hotmail.com>
2016-02-26 00:15:23 +1000

    database comfig changes and new migration files.

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'database'  => env('DB_DATABASE', 'grabmii'),
+            'username'  => env('DB_USERNAME', 'grabmii'),
+            'password'  => env('DB_PASSWORD', 'grabmii'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
